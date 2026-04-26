8ed1f29af948ffa2ff4b63f8a23b64fecce4b2a6
AnandInamdar180 <inamdaranand6@gmail.com>
2026-04-26 18:55:24 +0530

    fixed cart slice reducers

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
+// Optional: keep default export in case other files use it
+export default store;
